43005ec65f58d2a21a8e7f423f773fc4e425a512
David Granger <dpgranger8@gmail.com>
2023-05-15 09:30:57 -0600

    Initial Commit

---
+++ Test Driven Lab P2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Driven Lab P2
+//
+//  Created by David Granger on 5/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test Driven Lab P2UITests/Test_Driven_Lab_P2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test_Driven_Lab_P2UITestsLaunchTests.swift
+//  Test Driven Lab P2UITests
+//
+//  Created by David Granger on 5/15/23.
+//
+
+import XCTest
+
+final class Test_Driven_Lab_P2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
